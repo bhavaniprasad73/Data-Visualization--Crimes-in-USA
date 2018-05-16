175b600d91fc26baa42dbb7f9662897c4967659e
Bhavani Prasad Rao Ejanthkar <35183461+bhavaniprasad73@users.noreply.github.com>
2018-05-16 00:44:57 -0400

    USmaps
    USmaps

---
+++ US2014Crimehistogram.js
@@ -0,0 +1,215 @@
+window.histograms = {"states":[
+  {
+   "hits": 2210301, 
+   "name": "CA"
+  }, 
+  {
+   "hits": 832009, 
+   "name": "NY"
+  }, 
+  {
+   "hits": 306066, 
+   "name": "MA"
+  }, 
+  {
+   "hits": 1853672, 
+   "name": "TX"
+  }, 
+  {
+   "hits": 639037, 
+   "name": "IL"
+  }, 
+  {
+   "hits": 564707, 
+   "name": "WA"
+  }, 
+  {
+   "hits": 1355276, 
+   "name": "FL"
+  }, 
+  {
+   "hits": 577301, 
+   "name": "PA"
+  }, 
+  {
+   "hits": 358533, 
+   "name": "VA"
+  }, 
+  {
+   "hits": 357390, 
+   "name": "NJ"
+  }, 
+  {
+   "hits": 267881, 
+   "name": "OR"
+  }, 
+  {
+   "hits": 714282, 
+   "name": "OH"
+  }, 
+  {
+   "hits": 495415, 
+   "name": "MI"
+  }, 
+  {
+   "hits": 306788, 
+   "name": "CO"
+  }, 
+  {
+   "hits": 355505, 
+   "name": "MD"
+  }, 
+  {
+   "hits": 638262, 
+   "name": "NC"
+  }, 
+  {
+   "hits": 758411, 
+   "name": "GA"
+  }, 
+  {
+   "hits": 277225, 
+   "name": "MN"
+  }, 
+  {
+   "hits": 482012, 
+   "name": "AZ"
+  }, 
+  {
+   "hits": 399680, 
+   "name": "IN"
+  }, 
+  {
+   "hits": 275971, 
+   "name": "WI"
+  }, 
+  {
+   "hits": 408266, 
+   "name": "MO"
+  }, 
+  {
+   "hits": 483575, 
+   "name": "TN"
+  }, 
+  {
+   "hits": 156381, 
+   "name": "CT"
+  }, 
+  {
+   "hits": 85041, 
+   "name": "DC"
+  }, 
+  {
+   "hits": 184963, 
+   "name": "UT"
+  }, 
+  {
+   "hits": 173838, 
+   "name": "NM"
+  }, 
+  {
+   "hits": 181406, 
+   "name": "KS"
+  }, 
+  {
+   "hits": 219996, 
+   "name": "KY"
+  }, 
+  {
+   "hits": 268807, 
+   "name": "OK"
+  }, 
+  {
+   "hits": 386208, 
+   "name": "SC"
+  }, 
+  {
+   "hits": 371712, 
+   "name": "LA"
+  }, 
+  {
+   "hits": 186346, 
+   "name": "NV"
+  }, 
+  {
+   "hits": 147980, 
+   "name": "IA"
+  }, 
+  {
+   "hits": 58066, 
+   "name": "NH"
+  }, 
+  {
+   "hits": 351053, 
+   "name": "AL"
+  }, 
+  {
+   "hits": 228691, 
+   "name": "AR"
+  }, 
+  {
+   "hits": 56607, 
+   "name": "ME"
+  }, 
+  {
+   "hits": 99156, 
+   "name": "HI"
+  }, 
+  {
+   "hits": 106068, 
+   "name": "NE"
+  }, 
+  {
+   "hits": 68221, 
+   "name": "ID"
+  }, 
+  {
+   "hits": 50951, 
+   "name": "RI"
+  }, 
+  {
+   "hits": 20832, 
+   "name": "VT"
+  }, 
+  {
+   "hits": 58077, 
+   "name": "MT"
+  }, 
+  {
+   "hits": 88787, 
+   "name": "WV"
+  }, 
+  {
+   "hits": 65240, 
+   "name": "DE"
+  }, 
+  {
+   "hits": 50589, 
+   "name": "AK"
+  }, 
+  {
+   "hits": 191146, 
+   "name": "MS"
+  }, 
+  {
+   "hits": 25395, 
+   "name": "WY"
+  }, 
+  {
+   "hits": 38039, 
+   "name": "SD"
+  }, 
+  {
+   "hits": 35964, 
+   "name": "ND"
+  }, 
+  {
+   "hits": 107980, 
+   "name": "PR"
+  }, 
+  {
+   "hits": 2, 
+   "name": "AS"
+  }
+ ]
+ }
